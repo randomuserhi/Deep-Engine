e3dd8f6b20ebecc71d01b1ea1dafae89c84c59ee
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-02-10 13:49:28 +0000

    fix: bug with quaternions when using 16 bit floats due to inaccuracy

--- DeepEngine/Deep/Deep/Net/PacketReader.cpp
+++ DeepEngine/Deep/Deep/Net/PacketReader.cpp
@@ -12,30 +12,35 @@ namespace Deep {
     Quaternion PacketReader::ReadQuaternion() {
         uint8 i = ReadByte();
         Quaternion q{};
+        float32 norm;
         switch (i) {
         case 0:
             q.y = ReadFloat();
             q.z = ReadFloat();
             q.w = ReadFloat();
-            q.x = std::sqrtf(1.0f - q.y * q.y - q.z * q.z - q.w * q.w);
+            norm = 1.0f - q.y * q.y - q.z * q.z - q.w * q.w;
+            q.x = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         case 1:
             q.x = ReadFloat();
             q.z = ReadFloat();
             q.w = ReadFloat();
-            q.y = std::sqrtf(1.0f - q.x * q.x - q.z * q.z - q.w * q.w);
+            norm = 1.0f - q.x * q.x - q.z * q.z - q.w * q.w;
+            q.y = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         case 2:
             q.x = ReadFloat();
             q.y = ReadFloat();
             q.w = ReadFloat();
-            q.z = std::sqrtf(1.0f - q.x * q.x - q.y * q.y - q.w * q.w);
+            norm = 1.0f - q.x * q.x - q.y * q.y - q.w * q.w;
+            q.z = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         case 3:
             q.x = ReadFloat();
             q.y = ReadFloat();
             q.z = ReadFloat();
-            q.w = std::sqrtf(1.0f - q.x * q.x - q.y * q.y - q.z * q.z);
+            norm = 1.0f - q.x * q.x - q.y * q.y - q.z * q.z;
+            q.w = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         }
         return q;
@@ -44,30 +49,35 @@ namespace Deep {
     Quaternion PacketReader::ReadHalfQuaternion() {
         uint8 i = ReadByte();
         Quaternion q{};
+        float32 norm;
         switch (i) {
         case 0:
             q.y = ReadHalf();
             q.z = ReadHalf();
             q.w = ReadHalf();
-            q.x = std::sqrtf(1.0f - q.y * q.y - q.z * q.z - q.w * q.w);
+            norm = 1.0f - q.y * q.y - q.z * q.z - q.w * q.w;
+            q.x = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         case 1:
             q.x = ReadHalf();
             q.z = ReadHalf();
             q.w = ReadHalf();
-            q.y = std::sqrtf(1.0f - q.x * q.x - q.z * q.z - q.w * q.w);
+            norm = 1.0f - q.x * q.x - q.z * q.z - q.w * q.w;
+            q.y = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         case 2:
             q.x = ReadHalf();
             q.y = ReadHalf();
             q.w = ReadHalf();
-            q.z = std::sqrtf(1.0f - q.x * q.x - q.y * q.y - q.w * q.w);
+            norm = 1.0f - q.x * q.x - q.y * q.y - q.w * q.w;
+            q.z = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         case 3:
             q.x = ReadHalf();
             q.y = ReadHalf();
             q.z = ReadHalf();
-            q.w = std::sqrtf(1.0f - q.x * q.x - q.y * q.y - q.z * q.z);
+            norm = 1.0f - q.x * q.x - q.y * q.y - q.z * q.z;
+            q.w = std::sqrtf(norm > 1.0f ? 1.0f : norm < 0.0f ? 0.0f : norm);
             break;
         }
         return q;
