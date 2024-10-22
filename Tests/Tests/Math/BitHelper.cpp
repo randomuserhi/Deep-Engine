4fc68b8e07fd33a8f24924f02032ff5ac3ba3c23
randomuserhi <magicalworld.nano@gmail.com>
2024-10-22 16:39:55 +0100

    chore: BitHelper tests

---
+++ Tests/Tests/Math/BitHelper.cpp
@@ -0,0 +1,11 @@
+#include "../pch.h"
+#include "Deep.h"
+#include "Deep/BitHelper.h"
+
+TEST(BitHelper, NumberTrailingZeros) {
+    EXPECT_EQ(Deep::NumTrailingZeros(0u), 32u);
+    EXPECT_EQ(Deep::NumTrailingZeros(1u), 0u);
+    EXPECT_EQ(Deep::NumTrailingZeros(2u), 1u);
+    EXPECT_EQ(Deep::NumTrailingZeros(4u), 2u);
+    EXPECT_EQ(Deep::NumTrailingZeros(0xFFFFFFFFu), 0u);
+}
--- Tests/Tests/Math/Mat4.cpp
+++ Tests/Tests/Math/Mat4.cpp
@@ -2,18 +2,6 @@
 #include "Deep.h"
 #include "Deep/Math.h"
 
-// TODO(randomuserhi): Move BitHelper tests elsewhere...
-#include "Deep/BitHelper.h"
-#include <bitset>
-
-TEST(BitHelper, NumberTrailingZeros) {
-    EXPECT_EQ(Deep::NumTrailingZeros(0u), 32u);
-    EXPECT_EQ(Deep::NumTrailingZeros(1u), 0u);
-    EXPECT_EQ(Deep::NumTrailingZeros(2u), 1u);
-    EXPECT_EQ(Deep::NumTrailingZeros(4u), 2u);
-    EXPECT_EQ(Deep::NumTrailingZeros(0xFFFFFFFFu), 0u);
-}
-
 TEST(Mat4, Equality) {
     const Deep::Mat4 a{
         1, 0, 0, 0,
