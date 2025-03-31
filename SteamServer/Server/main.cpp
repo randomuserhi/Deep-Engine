2fd2a64daf1763866aca23dcc571f70d423f6595
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2025-03-31 10:49:37 +0100

    chore: Add Steamworks API

---
+++ SteamServer/Server/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include <Deep.h>
+#include <Deep/Memory.h>
+#include <Deep/Math.h>
+#include <Deep/Time.h>
+
+#include <steam/steam_api.h>
+#include <steam/isteamnetworkingsockets.h>
+
+// https://stackoverflow.com/a/62047818/9642458
+#include <chrono>
+#include <thread>
+#include <vector>
+
+class Net {
+    // TODO(randomuserhi):
+};
+
+static Deep_Inline void NetTick(Net& net) {
+    // TODO(randomuserhi):
+
+    int32 bytesReceived;
+    while (false) {
+        std::cout << "Received " << bytesReceived << " bytes.\n";
+    }
+}
+
+static Deep_Inline void Tick(Net& net, float32 dt) {}
+
+int main() {
+    SteamAPI_Init();
+
+    // TODO(randomuserhi): Init netcode
+    Net net;
+
+    // Main Tick Loop
+    constexpr const int32 tickRate = 60; // Server runs at 60 ticks per second
+    constexpr const int32 totalFrameTime = 1000 / tickRate;
+    long long start = Deep::Epoch();
+    while (true) {
+        long long now = Deep::Epoch();
+        float32 dt = static_cast<float>(now - start) / 1000.0f;
+        start = now;
+
+        // Network tick
+        NetTick(net);
+
+        // Perform tick
+        Tick(net, dt);
+
+        long long end = Deep::Epoch();
+        long long elapsed = end - start;
+
+        // Spin lock to wait until next frame
+        using namespace std::chrono_literals;
+        long long timeleft = totalFrameTime - elapsed;
+        if (timeleft < 0) {
+            std::cout << "Tick took longer than expected! [" << elapsed << "ms]\n";
+            timeleft = 0ll;
+        }
+        while (Deep::Epoch() < end + timeleft) {
+            // std::this_thread::sleep_for(std::chrono::microseconds{ 1 });
+        }
+    };
+
+    SteamAPI_Shutdown();
+
+    return 0;
+}
